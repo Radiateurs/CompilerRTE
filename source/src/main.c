817f4fb435aef509ca2aed44f5dda8a5d032acc7
Radiateurs <pierre-marie.danieau@epitech.eu>
2017-04-06 11:49:49 +0200

    Prepare Repo

---
+++ source/src/main.c
@@ -0,0 +1,13 @@
+/*
+** Created by Pierre-Marie Danieau for CASedi.
+**
+** Started the 06-04-2017
+ */
+
+#include	<stdio.h>
+
+int		main(int argc, char *argv[], char *env[])
+{
+  puts("Hello world!");
+  return (0);
+}
